ab9b9ee38c776a46452a1a3188fc59ad8164256c
Hector Cardona <hectorjulian98@gmail.com>
2024-03-30 19:29:30 -0500

    getPath method created

--- src/components/TableMatrix.jsx
+++ src/components/TableMatrix.jsx
@@ -1,18 +1,20 @@
-import React, {useState, useEffect} from 'react'
-import {Node} from "./Node"
+import React, { useState, useEffect } from 'react'
+import { dijkstra } from '../utils/algorithms/dijkstra';
+import { Node } from "./Node"
 
-export function TableMatrix(){
+export function TableMatrix() {
 
     const [matrix, setMatrix] = useState([])
     const [mouseIsPressed, setMousePress] = useState(false);
     const NUM_ROWS = 20;
     const NUM_COLS = 20;
-    const TABLE_SIZE = NUM_COLS*40
-    const INIT_POS = [0,7]
-    const END_POS = [15,16]
+    const TABLE_SIZE = NUM_COLS * 40
+    const TABLE_CLASS = `w-[${TABLE_SIZE}px]`
+    const INIT_POS = [0, 7]
+    const END_POS = [15, 16]
 
-    function createNode(row,col){
-        return { 
+    function createNode(row, col) {
+        return {
             row,
             col,
             isWall: false,
@@ -21,32 +23,39 @@ export function TableMatrix(){
         }
     }
 
-    function getNewGridWithToggledWall(row, col){
+    function getNewGridWithToggledWall(row, col) {
         const tempGrid = [...matrix]
         const node = tempGrid[row][col]
         const newNode = {
             ...node,
-            isWall : !node.isWall,
+            isWall: !node.isWall,
         }
         tempGrid[row][col] = newNode
         return tempGrid
     }
 
-    function handleClick(row, col){
+    function findShortestPath() {
+        const startNode = matrix[INIT_POS[0]][INIT_POS[1]]
+        const endNode = matrix[END_POS[0]][END_POS[1]]
+        const path = dijkstra(matrix, startNode, endNode)
+        console.log(path)
+    }
+
+    function handleClick(row, col) {
         const tempGrid = getNewGridWithToggledWall(row, col)
         setMatrix(tempGrid)
     }
 
-    function onMouseEnter(row, col){
-        if(!mouseIsPressed) return
+    function onMouseEnter(row, col) {
+        if (!mouseIsPressed) return
         handleClick(row, col)
     }
 
-    function initGrid(){
+    function initGrid() {
         const tempMatrix = []
-        for(let row = 0 ; row < NUM_ROWS ; row++){
+        for (let row = 0; row < NUM_ROWS; row++) {
             let currentRow = []
-            for(let col = 0; col < NUM_COLS ; col++){
+            for (let col = 0; col < NUM_COLS; col++) {
                 currentRow.push(createNode(row, col))
             }
             tempMatrix.push(currentRow)
@@ -60,13 +69,13 @@ export function TableMatrix(){
 
     console.log(matrix)
 
-    return(
-        <div className={`w-[${TABLE_SIZE}px] my-0 mx-auto`}>
+    return (
+        <div className={`${TABLE_CLASS} my-0 mx-auto`}>
             {
                 matrix.map((row, rowIdx) => (
                     <div className='grid grid-cols-20' key={rowIdx}>
                         {row.map((node, colIdx) => (
-                            <Node 
+                            <Node
                                 key={colIdx}
                                 col={node.col}
                                 row={node.row}
@@ -78,14 +87,22 @@ export function TableMatrix(){
                                 handleMouseUp={() => {
                                     setMousePress(false)
                                 }}
-                                handleMouseEnter={()=>onMouseEnter(node.row, node.col)}
+                                handleMouseEnter={() => onMouseEnter(node.row, node.col)}
                                 isInit={INIT_POS[0] === node.row && INIT_POS[1] === node.col}
                                 isEnd={END_POS[0] === node.row && END_POS[1] === node.col}
                             />
                         ))}
                     </div>
-                    ))
+                ))
             }
-        </div>    
+            <div className='m-2 flex place-content-center'>
+                <button 
+                    onClick={() => {findShortestPath()}}
+                    type="button" 
+                    className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    Sort
+                </button>
+            </div>
+        </div>
     )
 }
--- src/utils/algorithms/dijkstra.js
+++ src/utils/algorithms/dijkstra.js
@@ -1,10 +1,10 @@
-import PriorityQueue from './priorityQueue';
+import PriorityQueue from '../structures/PriorityQueue.js';
 
 function getNeighbors(grid, row, col){
     const d4x = [0, 1, 0, -1]
     const d4y = [-1, 0, 1, 0]
-    const numRows = matrix.length
-    const numCols = matrix[0].length
+    const numRows = grid.length
+    const numCols = grid[0].length
     const neighbors = []
     for(let i = 0; i < 4; i++){
         const newRow = row + d4x[i]
@@ -16,27 +16,45 @@ function getNeighbors(grid, row, col){
     return neighbors
 }
 
+function getPath(predecessors, end_node) {
+    const path = []
+    let currentRow = end_node.row
+    let currentCol = end_node.col
+    while (predecessors[currentRow][currentCol] !== null) {
+        path.push({row: currentRow, col: currentCol})
+        const {row, col} = predecessors[currentRow][currentCol]
+        currentRow = row
+        currentCol = col    
+    }
+    path.push({row: currentRow, col: currentCol})
+    return path.reverse()
+
+}
+
 export function dijkstra(grid, start_node, end_node){
-    const numRows = matrix.length
-    const numCols = matrix[0].length
+    const numRows = grid.length
+    const numCols = grid[0].length
     const distances = Array.from({length: numRows}, () => Array.from({length: numCols}, () => Infinity))
     const predecessors = Array.from({length: numRows}, () => Array.from({length: numCols}, () => null))
-    const pq = PriorityQueue()
+    const pq = new PriorityQueue()
     distances[start_node.row][start_node.col] = 0
     pq.enqueue(start_node)
     while(pq.heap.length > 0){
         const {row, col, priority, isAWall} = pq.dequeue()
+        console.log(row,col)
         if(priority > distances[row][col]) continue;
         if(row === end_node.row && col === end_node.col) return getPath(predecessors, end_node)
         const neighbors = getNeighbors(grid, row, col)
-        for(const [neighborRow, neighborCol, weight] of neighbors){
-            const distance = distances[row][col] + weight
-            if(distance < distance[neighborRow][neighborCol]){
-                //End of the day, we want to update the distance and the predecessor
+        for(const neighbor of neighbors){
+            const distance = distances[row][col] + neighbor.priority
+            if(distance < distance[neighbor.row][neighbor.col]){
+                distances[neighbor.row][neighbor.col] = distance
+                predecessors[neighbor.row][neighbor.col] = {row, col}
+                grid[neighbor.row][neighbor.col].priority = distance
+                pq.enqueue(grid[neighbor.row][neighbor.col])
             }
 
         }
     }   
-
-
+    return null
 }
